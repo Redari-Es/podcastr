5fadcd0266de6fc81c90763b97048924dbec811f
Redari-Es <905422030@qq.com>
2024-08-30 20:50:55 +0800

    first commit

---
+++ components/RightSidbar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RightSidbar = () => {
+    return (
+        <section className='right_sidebar text-white-1'>RightSidbar</section>
+    )
+}
+
+export default RightSidbar
